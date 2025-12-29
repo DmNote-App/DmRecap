d3266f4f6483f9379b0bbe81f7040e2763895399
연우 <imyeonu@pm.me>
2025-12-29 09:45:16 +0900

    fix 아오

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "https://dm-recap.vercel.app" : undefined,
   reactStrictMode: true,
   images: {
     remotePatterns: [
